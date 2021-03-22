72ac06f121f5cf0a480b9f6fecb610fc00e7c066
David Grimmett <davidsgrimmett@gmail.com>
2021-03-21 21:12:16 -0400

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,10 +1,10 @@
 // Creating map object
 var myMap = L.map("map", {
-  center: [38.89511, -77.03637],
+  center: [40.52, -87.2123],
   zoom: 6
 });
 
-//Gray Mapbox background
+//Mapbox background
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
@@ -12,17 +12,13 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Load in geojson data
+// Load geojson data
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// var geojson;
 
 //Grab data with d3
 d3.json(geoData, function (data) {
 
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. We pass the magnitude of the earthquake into two separate functions
-  // to calculate the color and radius.
+// This function returns the the magnitude of the earthquake into two separate functions.
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -35,7 +31,7 @@ d3.json(geoData, function (data) {
     };
   }
 
-  // This function determines the color of the marker based on the magnitude of the earthquake.
+  // This function determines the color of the marker based on the magnitude.
   function getColor(magnitude) {
     switch (true) {
       case magnitude > 5:
@@ -53,8 +49,7 @@ d3.json(geoData, function (data) {
     }
   }
 
-  // This function determines the radius of the earthquake marker based on its magnitude.
-  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+  // This function determines the radius of the marker based on its magnitude.
   function getRadius(magnitude) {
     if (magnitude === 0) {
       return 1;
@@ -62,7 +57,6 @@ d3.json(geoData, function (data) {
 
     return magnitude * 4;
   }
-
 
   L.geoJson(data, {
 
@@ -72,8 +66,6 @@ d3.json(geoData, function (data) {
 
     style: styleInfo,
 
-
-
     // Binding a pop-up to each layer
     onEachFeature: function (feature, layer) {
 
@@ -81,29 +73,25 @@ d3.json(geoData, function (data) {
     }
   }).addTo(myMap);
 
-  //add legend on Bottom Right Corner
+  // Adding Legend
   var legend = L.control({ position: 'bottomright' });
 
   legend.onAdd = function (map) {
-    //Dom Utility that puts legend into DIV & Info Legend
     var div = L.DomUtil.create('div', 'info legend'),
-      //Magnitude Grades, stops at 5 magnitude
       grades = [0, 1, 2, 3, 4, 5];
 
-    //Legend Label Earthquake <break> Magnitude  
+    // Legend Label
     div.innerHTML = 'Eathquake<br>Magnitude<br><hr>'
 
-    // loop through our density intervals and generate a label with a colored square for each interval
+    // Loop through density intervals
     for (var i = 0; i < grades.length; i++) {
       div.innerHTML +=
-        //HTML code with nbs(non-breaking space) and ndash
         '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
         grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
-
     return div;
   };
-  //Adds Legend to myMap
+  //Adds Legend
   legend.addTo(myMap);
 
 });
